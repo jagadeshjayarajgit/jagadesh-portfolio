6d7ff992f12d17819f584375e491ae65e64b6def
JAGADESH J <108328300+jagadeshjayarajgit@users.noreply.github.com>
2026-05-18 10:47:19 +0000

    Changes

--- src/app/layouts/project-page/project-page.component.ts
+++ src/app/layouts/project-page/project-page.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+interface Project {
+  name: string;
+  category: string;
+  image: string;
+  url: string | null;
+}
 
 @Component({
   selector: 'app-project-page',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './project-page.component.html',
   styleUrl: './project-page.component.css'
 })
 export class ProjectPageComponent {
 
+  projects: Project[] = [
+    {
+      name: 'StryPers Hub',
+      category: 'Web development',
+      image: 'https://image.thum.io/get/width/600/crop/380/noanimate/https://strypershub.vercel.app/profilepage',
+      url: 'https://strypershub.vercel.app/profilepage',
+    },
+  ];
 }
